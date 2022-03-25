cdc81d79d0abf84ef29c8071aa577fe9ab35b732
DarkedgeTH <darkedgeth@gmail.com>
2022-03-24 20:25:05 -0400

    set up reducers for Quiz

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -4,6 +4,9 @@ import { combineReducers } from 'redux'
 import {
   MOVE_CLOCKWISE,
   MOVE_COUNTERCLOCKWISE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  SET_QUIZ_INTO_STATE
 } from './action-types';
 
 const initialWheelState = 0
@@ -28,17 +31,32 @@ function wheel(state = initialWheelState, action) {
 
 const initialQuizState = null
 function quiz(state = initialQuizState, action) {
-  return state
+  switch (action.type) {
+    case SET_QUIZ_INTO_STATE:
+      return action.payload
+    default:
+      return state
+  }
 }
 
 const initialSelectedAnswerState = null
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  switch (action.type) {
+    case SET_SELECTED_ANSWER:
+      return action.payload
+    default:
+      return state
+  }
 }
 
 const initialMessageState = ''
 function infoMessage(state = initialMessageState, action) {
-  return state
+  switch (action.type) {
+    case SET_INFO_MESSAGE:
+      return action.payload
+    default:
+      return state
+  }
 }
 
 const initialFormState = {
